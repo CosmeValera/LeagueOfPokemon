915355de53a352147cc65306dcb60998a969d462
CosmeValera <cosmevalerareales@gmail.com>
2021-06-09 19:24:18 +0200

    change pom so tests work

--- src/test/java/tests/TeemoTest.java
+++ src/test/java/tests/TeemoTest.java
@@ -1,18 +1,25 @@
-//package tests;
-//
-//import org.junit.jupiter.api.Test;
-//
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//import org.junit.jupiter.api.BeforeEach;
-//
-//public class TeemoTest {
-//
+package tests;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TeemoTest {
+
 //    @BeforeEach
 //    public void init() {
 //    }
-//
-//    @Test
-//    public void should_return_empty_score() {
-//        assertEquals(0, 0);
-//    }
-//}
+
+    @Test
+    public void should_return_empty_score() {
+        assertEquals(0, 0);
+    }
+    @Test
+    public void should_return_one_score() {
+        assertEquals(1, 0);
+    }
+    @Test
+    public void should_return_340_score() {
+        assertEquals(340, 340);
+    }
+}
